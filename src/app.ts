04afd0bf839a34b8dd44974dd45df53b24eb7ffb
Ayush Chauhan <ayush2004chauhan2526@gmail.com>
2026-05-07 22:54:02 +0530

    add the new nsInclude flag in mongorestore command

--- src/app.ts
+++ src/app.ts
@@ -4,106 +4,107 @@ import fs from "fs";
 import path from "path";
 import { logger } from "./logger";
 import {
-    backupDir,
-    getBackedUpDBs,
-    getConnections,
-    getTimestamp,
-    listDatabases,
-    runCommand,
+	backupDir,
+	getBackedUpDBs,
+	getConnections,
+	getTimestamp,
+	listDatabases,
+	runCommand,
 } from "./helpers";
 
 async function backupDatabase(uri: string, dbName: string): Promise<void> {
-    const timestamp = getTimestamp();
-    const archivePath = path.join(
-        backupDir,
-        `${dbName}-${timestamp}.archive.gz`,
-    );
+	const timestamp = getTimestamp();
+	const archivePath = path.join(
+		backupDir,
+		`${dbName}-${timestamp}.archive.gz`,
+	);
 
-    logger.info(`Starting backup for DB: ${dbName}`);
-    logger.info(`Archive file: ${archivePath}`);
+	logger.info(`Starting backup for DB: ${dbName}`);
+	logger.info(`Archive file: ${archivePath}`);
 
-    await runCommand("mongodump", [
-        `--uri="${uri}"`,
-        `--db=${dbName}`,
-        `--archive="${archivePath}"`,
-        "--gzip",
-    ]);
+	await runCommand("mongodump", [
+		`--uri="${uri}"`,
+		`--db=${dbName}`,
+		`--archive="${archivePath}"`,
+		"--gzip",
+	]);
 
-    logger.info("Backup completed successfully.");
+	logger.info("Backup completed successfully.");
 }
 
 async function restoreDatabase(
-    uri: string,
-    archiveFile: string,
-    sourceDb: string,
-    targetDb: string,
+	uri: string,
+	archiveFile: string,
+	sourceDb: string,
+	targetDb: string,
 ): Promise<void> {
-    const archivePath = path.join(backupDir, archiveFile);
+	const archivePath = path.join(backupDir, archiveFile);
 
-    logger.info(`Restoring archive: ${archiveFile}`);
-    logger.info(`Renaming ${sourceDb} → ${targetDb}`);
+	logger.info(`Restoring archive: ${archiveFile}`);
+	logger.info(`Renaming ${sourceDb} → ${targetDb}`);
 
-    await runCommand("mongorestore", [
-        `--uri=${uri}`,
-        "--drop",
-        "--gzip",
-        `--archive=${archivePath}`,
-        `--nsFrom=${sourceDb}.*`,
-        `--nsTo=${targetDb}.*`,
-    ]);
+	await runCommand("mongorestore", [
+		`--uri=${uri}`,
+		"--drop",
+		"--gzip",
+		`--archive=${archivePath}`,
+		`--nsInclude=${sourceDb}.*`,
+		`--nsFrom=${sourceDb}.*`,
+		`--nsTo=${targetDb}.*`,
+	]);
 
-    logger.info("Restore completed successfully.");
+	logger.info("Restore completed successfully.");
 }
 
 async function main(): Promise<void> {
-    try {
-        const connections = getConnections();
-        if (!connections.length) {
-            logger.error("No DB connection strings found in .env");
-            return;
-        }
+	try {
+		const connections = getConnections();
+		if (!connections.length) {
+			logger.error("No DB connection strings found in .env");
+			return;
+		}
 
-        const actions = {
-            backup: "Backup",
-            restore: "Restore",
-        };
+		const actions = {
+			backup: "Backup",
+			restore: "Restore",
+		};
 
-        const action = await select({
-            message: "Choose an action:",
-            choices: [actions.backup, actions.restore],
-        });
+		const action = await select({
+			message: "Choose an action:",
+			choices: [actions.backup, actions.restore],
+		});
 
-        const connection = await select({
-            message: "Select MongoDB connection:",
-            choices: connections.map((c) => ({ name: c.name, value: c.uri })),
-        });
+		const connection = await select({
+			message: "Select MongoDB connection:",
+			choices: connections.map((c) => ({ name: c.name, value: c.uri })),
+		});
 
-        const dbs =
-            action === actions.backup
-                ? await listDatabases(connection)
-                : getBackedUpDBs();
-        const dbName = await select({
-            message: "Select database:",
-            choices: dbs,
-        });
+		const dbs =
+			action === actions.backup
+				? await listDatabases(connection)
+				: getBackedUpDBs();
+		const dbName = await select({
+			message: "Select database:",
+			choices: dbs,
+		});
 
-        if (action === "Backup") {
-            await backupDatabase(connection, dbName);
-        } else {
-            const targetDb = await input({
-                message: "Enter new DB name:",
-                required: true,
-            });
-            await restoreDatabase(
-                connection,
-                dbName,
-                dbName.slice(0, -11 - 25), // 11 = .archive.tz, 25 = '-' + timestamp
-                targetDb,
-            );
-        }
-    } catch (error: any) {
-        logger.error(`Application error: ${error.message}`);
-    }
+		if (action === "Backup") {
+			await backupDatabase(connection, dbName);
+		} else {
+			const targetDb = await input({
+				message: "Enter new DB name:",
+				required: true,
+			});
+			await restoreDatabase(
+				connection,
+				dbName,
+				dbName.slice(0, -11 - 25), // 11 = .archive.tz, 25 = '-' + timestamp
+				targetDb,
+			);
+		}
+	} catch (error: any) {
+		logger.error(`Application error: ${error.message}`);
+	}
 }
 
 main();
